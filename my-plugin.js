a3585fe03feb56fd090c400c026063fc63d2f928
phuhung273 <phuhung.tranpham@gmail.com>
2023-06-19 20:46:41 +0700

    hello world plugin

---
+++ my-plugin.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class MyPlugin {
+    constructor(serverless) {
+        this.serverless = serverless;
+        this.hooks = {
+            initialize: () => this.init(),
+        };
+    }
+
+    init() {
+        // console.log('Serverless instance: ', this.serverless);
+
+        // `serverless.service` contains the (resolved) serverless.yml config
+        const service = this.serverless.service;
+        console.log('Provider name: ', service.provider.name);
+        console.log('Functions: ', service.functions);
+    }
+}
+
+module.exports = MyPlugin;
